bba0d28e975fcc4044f9c8ea436b7957382b5e25
Prateek <pgupta@grepruby.com>
2018-02-21 13:35:36 +0530

    DSStop contract added

---
+++ contracts/DSStop.sol
@@ -0,0 +1,22 @@
+import "./DSAuth.sol";
+import "./DSNote.sol";
+
+
+contract DSStop is DSAuth, DSNote {
+
+    bool public stopped;
+
+    modifier stoppable {
+        assert(!stopped);
+        _;
+    }
+
+    function stop() auth note {
+        stopped = true;
+    }
+
+    function start() auth note {
+        stopped = false;
+    }
+
+}
